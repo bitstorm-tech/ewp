7b7728409854a47c6694ca78a7b1b642d15c1697
Josef Bauer <bauerjoe@gmx.de>
2018-08-29 19:03:49 +0200

    Added some function skeletons

--- main.go
+++ main.go
@@ -2,16 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
 )
 
+var configFileName = ".proxy-manager.conf"
+
+type config struct {
+	proxyHost string
+	proxyPort int
+	proxyUser string
+}
+
 func main() {
-	fmt.Println("Ach so ging das :)")
+	fmt.Printf("Home directory: %v\n", getHomeDirectory())
 }
 
-func getConfig() {
-
+func createDefaultConfig() (config, error) {
+	return config{}, nil
 }
 
-func createInitialConfig() {
+func getHomeDirectory() string {
+	u, err := user.Current()
+	if err != nil {
+		fmt.Println("Can't find user home directory, use working directory")
+		return "."
+	}
 
+	return u.HomeDir
+}
+
+func readConfigFile() config {
+	c := getHomeDirectory() + configFileName
+	_, err := os.OpenFile(c, os.O_RDWR, 0755)
+	if err != nil {
+		fmt.Println("No config file found, create a default one")
+		if d, err := createDefaultConfig(); err == nil {
+			return d
+		}
+	}
+
+	return config{}
 }
