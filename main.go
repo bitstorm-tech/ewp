3ffe99daef204be5d9214793f8901e3f9e286a08
Josef Bauer <bauerjoe@gmx.de>
2018-09-01 00:03:44 +0200

    Read from stdin and write the config file

--- main.go
+++ main.go
@@ -1,46 +1,93 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/user"
+	"strconv"
+	"strings"
 )
 
-var configFileName = ".proxy-manager.conf"
-
 type config struct {
-	proxyHost string
-	proxyPort int
-	proxyUser string
+	ProxyHost  string
+	ProxyPort  int
+	ProxyUser  string
+	Default    bool
+	TTLSeconds int
 }
+
+var configFileName = ".proxy-manager.json"
+var delim = byte('\n')
+var configs = make(map[int]config)
+var stdin = bufio.NewReader(os.Stdin)
 
 func main() {
-	fmt.Printf("Home directory: %v\n", getHomeDirectory())
+	c := config{}
+	c.ProxyUser = readString("Proxy User (optional): ")
+	c.ProxyHost = readString("Proxy Host: ")
+	c.ProxyPort = readInt("Proxy Port (80): ", 80)
+	c.Default = readBool("Default proxy (y/N): ", false)
+	c.TTLSeconds = readInt("TTL in seconds (optional): ", 0)
+	writeConfigToFile(c)
 }
 
-func createDefaultConfig() (config, error) {
-	return config{}, nil
+func readString(prompt string) string {
+	fmt.Print(prompt)
+	s, err := stdin.ReadString(delim)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return strings.Replace(s, "\n", "", -1)
+}
+
+func readInt(prompt string, d int) int {
+	s := readString(prompt)
+
+	if len(s) == 0 {
+		return d
+	}
+
+	i, err := strconv.Atoi(s)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func readBool(prompt string, d bool) bool {
+	s := readString(prompt)
+
+	if len(s) == 0 {
+		return d
+	}
+
+	return strings.EqualFold(s, "y")
 }
 
 func getHomeDirectory() string {
 	u, err := user.Current()
 	if err != nil {
-		fmt.Println("Can't find user home directory, use working directory")
+		fmt.Printf("Can't get user home directory, use current working directory (%v)\n", err)
 		return "."
 	}
 
 	return u.HomeDir
 }
 
-func readConfigFile() config {
-	c := getHomeDirectory() + configFileName
-	_, err := os.OpenFile(c, os.O_RDWR, 0755)
-	if err != nil {
-		fmt.Println("No config file found, create a default one")
-		if d, err := createDefaultConfig(); err == nil {
-			return d
-		}
-	}
+func writeConfigToFile(c config) {
+	f, err := os.Create("proxy-manager.json")
+	defer f.Close()
+	j, err := json.Marshal(c)
+	f.Write(j)
+	f.Sync()
 
-	return config{}
+	if err != nil {
+		panic(err)
+	}
 }
